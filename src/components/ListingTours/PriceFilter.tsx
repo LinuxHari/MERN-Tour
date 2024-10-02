7e5bc31efa4a86d174c49ddf6aaaf9bb291fbfbd
Linux Hari <vadiveluversionz1.0@gmail.com>
2024-10-02 22:48:47 +0530

    feat: price filter is done

--- src/components/ListingTours/PriceFilter.tsx
+++ src/components/ListingTours/PriceFilter.tsx
@@ -1,11 +1,21 @@
 import Accordion from "../Shared/Accordion/Accordion";
+import PriceRangeSlider from "rc-slider";
+import "rc-slider/assets/index.css";
+import { useState } from "react";
 
 type PriceFilterProps = {
-  setPriceRange: (minPrice: number, maxPrice: number) => void
-}
+  setPriceRange: (minPrice: number, maxPrice: number) => void;
+};
 
-const PriceFilter = ({setPriceRange}: PriceFilterProps) => {
-  const defaultRange = {minPrice: 1, maxPrice: 25000}
+const PriceFilter = ({ setPriceRange }: PriceFilterProps) => {
+  const defaultRange = { minPrice: 5, maxPrice: 10000 };
+
+  const [currentPriceRange, setCurrentRange] = useState([defaultRange.minPrice, defaultRange.maxPrice]);
+
+  const [currentMinPrice, currentMaxPrice] = currentPriceRange;
+
+  const handleRange = (range: number[]) => setPriceRange(range[0], range[1])
+
   return (
     <div className="sidebar__item">
       <Accordion>
@@ -17,16 +27,22 @@ const PriceFilter = ({setPriceRange}: PriceFilterProps) => {
           <Accordion.Content>
             <div className="pt-15">
               <div className="js-price-rangeSlider">
-                <div className="px-5">
-                  <div className="js-slider"></div>
+                <div className="mx-3">
+                <PriceRangeSlider
+                  min={defaultRange.minPrice}
+                  value={currentPriceRange}
+                  max={defaultRange.maxPrice}
+                  range={{ minCount: 2, maxCount: 2 }}
+                  onChange={(range) => setCurrentRange(range as number[])}
+                  onChangeComplete={(range) => handleRange(range as number[])}
+                />
                 </div>
-
                 <div className="d-flex justify-between mt-20">
                   <div className="">
                     <span className="">Price: </span>
-                    <span className="fw-500 js-lower">{defaultRange.minPrice}</span>
+                    <span className="fw-500 js-lower">${currentMinPrice}</span>
                     <span> - </span>
-                    <span className="fw-500 js-upper">{defaultRange.maxPrice}</span>
+                    <span className="fw-500 js-upper">${currentMaxPrice}</span>
                   </div>
                 </div>
               </div>
