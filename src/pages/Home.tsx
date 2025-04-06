74484e4dc48282a248e05ee9a91d54a16ee4f053
Linux Hari <vadiveluversionz1.0@gmail.com>
2025-04-06 23:13:55 +0530

    feat: animation on scroll home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,6 @@
+import {useEffect} from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import Advantages from "../components/Home/Advantages/Advantages";
 import Hero from "../components/Home/Hero/Hero";
 import OfferBanner from "../components/Home/OfferBanner/OfferBanner";
@@ -7,15 +10,56 @@ import TopTrending from "../components/Home/TopTrending/TopTrending";
 import TrendingDestinations from "../components/Home/TrendingDestinations/TrendingDestinations";
 
 const Home = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      once: false,
+      mirror: false,
+      offset: 120,
+      easing: "ease-in-out",
+    });
+
+    const handleResize = () => {
+      AOS.refresh();
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Clean up
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
-      <Hero />
-      <Advantages />
-      <TrendingDestinations />
-      <PopularTours />
-      <PopularThings />
-      <OfferBanner />
-      <TopTrending />
+      <div data-aos="fade-down">
+        <Hero />
+      </div>
+
+      <div data-aos="fade-up" data-aos-delay="100">
+        <Advantages />
+      </div>
+
+      <div data-aos="fade-up" data-aos-delay="200">
+        <TrendingDestinations />
+      </div>
+
+      <div data-aos="fade-right" data-aos-delay="100">
+        <PopularTours />
+      </div>
+
+      <div data-aos="fade-left" data-aos-delay="100">
+        <PopularThings />
+      </div>
+
+      <div data-aos="fade-down" data-aos-delay="200">
+        <OfferBanner />
+      </div>
+
+      <div data-aos="fade-up" data-aos-delay="200">
+        <TopTrending />
+      </div>
     </>
   );
 };
